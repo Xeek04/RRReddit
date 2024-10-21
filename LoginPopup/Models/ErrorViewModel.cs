0c2a0b0d4076a08f44e9ae3d006e7e8c257e5094
Xeek04 <101283100+Xeek04@users.noreply.github.com>
2024-10-21 14:41:20 -0500

    Delete LoginPopup directory

--- LoginPopup/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LoginPopup.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
