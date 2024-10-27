fa0d7e4908e88036a9a16e1ffe77a8328d5594e2
Coby Taylor <117447744+KingOuroboros@users.noreply.github.com>
2024-10-27 18:30:13 -0500

    Update HomeController.cs

--- LoginPopup/Controllers/HomeController.cs
+++ LoginPopup/Controllers/HomeController.cs
@@ -21,11 +21,7 @@ namespace LoginPopup.Controllers
         public IActionResult Logout()
         {
             HttpContext.Session.Remove("AccessToken");
-<<<<<<< HEAD
-            return RedirectToAction("Info");
-=======
             return RedirectToAction("Index");
->>>>>>> master
         }
 
         public IActionResult Index()
@@ -33,56 +29,13 @@ namespace LoginPopup.Controllers
             return View();
         }
 
-<<<<<<< HEAD
-        [HttpGet]
-=======
->>>>>>> master
         public IActionResult Create()
         {
             return View();
         }
 
         [HttpPost]
-<<<<<<< HEAD
-        public async Task<IActionResult> Create(LoginModel vm)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(vm);
-            }
-            try
-            {
-                var result = await _firebaseAuth.CreateUserWithEmailAndPasswordAsync(vm.EmailAddress, vm.Password);
-                await _firebaseAuth.SendEmailVerificationAsync(result.FirebaseToken);
-
-
-                string accessToken = result.FirebaseToken;
-
-                if (!string.IsNullOrEmpty(accessToken))
-                {
-                    HttpContext.Session.SetString("AccessToken", accessToken);
-                    return RedirectToAction("Index");
-                }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "Authentication failed. Invalid token.");
-                    return View(vm);
-                }
-            }
-            catch (FirebaseAuthException ex)
-            {
-                // Handle Firebase authentication exceptions
-                var errorMessage = GetFirebaseAuthErrorMessage(ex);
-                ModelState.AddModelError(string.Empty, errorMessage);
-                return View(vm);
-            }
-        }
-
         [HttpPost]
-        //[HttpPost]
-=======
-        [HttpPost]
->>>>>>> master
 
         private string GetFirebaseAuthErrorMessage(FirebaseAuthException ex)
         {
@@ -112,11 +65,7 @@ namespace LoginPopup.Controllers
                     _logger.LogError(error.ErrorMessage);
                 }
                 // Return the view with validation errors
-<<<<<<< HEAD
-                return View("LoginPartial", vm);
-=======
                 return View("create", vm);
->>>>>>> master
             }
 
             try
@@ -127,24 +76,14 @@ namespace LoginPopup.Controllers
 
                 if (!string.IsNullOrEmpty(accessToken))
                 {
-<<<<<<< HEAD
-                    //storing access token and email in session
                     HttpContext.Session.SetString("AccessToken", accessToken);
-                    HttpContext.Session.SetString("UserEmail", vm.EmailAddress);
-=======
-                    HttpContext.Session.SetString("AccessToken", accessToken);
->>>>>>> master
                     // Redirect to Index or show a success message
                     return RedirectToAction("Index");
                 }
                 else
                 {
                     ModelState.AddModelError(string.Empty, "Authentication failed. Invalid token.");
-<<<<<<< HEAD
-                    return View("LoginPartial", vm);
-=======
                     return View("create", vm);
->>>>>>> master
                 }
             }
             catch (FirebaseAuthException ex)
@@ -152,11 +91,7 @@ namespace LoginPopup.Controllers
                 // Handle Firebase authentication exceptions
                 var errorMessage = GetFirebaseAuthErrorMessage(ex);
                 ModelState.AddModelError(string.Empty, errorMessage);
-<<<<<<< HEAD
-                return View("LoginPartial", vm);
-=======
                 return View("create", vm);
->>>>>>> master
             }
         }
 
@@ -166,7 +101,18 @@ namespace LoginPopup.Controllers
             return View();
         }
 
+        public IActionResult Testing()  //controller for the Comment Testing by Senmi 
+        {
+            return View();
+        }
+
         public IActionResult Button()
+        {
+            return View();
+        }
+
+
+        public IActionResult Watchlist()
         {
             return View();
         }
@@ -194,34 +140,13 @@ namespace LoginPopup.Controllers
 
         public IActionResult AccountPage()
         {
-<<<<<<< HEAD
-            // Get the email from the session
-            var email = HttpContext.Session.GetString("UserEmail");
-
-            if (!string.IsNullOrEmpty(email))
-            {
-                // Extract the name from the email (everything before '@')
-                var name = email.Split('@')[0];
-
-                ViewData["AccountName"] = name;
-            }
-   
             return View();
         }
 
-
-=======
-            return View();
-        }
-
->>>>>>> master
         public IActionResult Testpage()
         {
             return View();
         }
-<<<<<<< HEAD
-=======
         // Other actions...
->>>>>>> master
     }
 }
