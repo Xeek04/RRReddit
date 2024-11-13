e5375b545ad55c7ba4d407311dee33797006be45
Coby Taylor <117447744+KingOuroboros@users.noreply.github.com>
2024-11-13 15:38:15 -0600

    Sprint 3

---
+++ LoginPopup/Models/Data/MongoDatabase.cs
@@ -0,0 +1,22 @@
+using MongoDB.Driver;
+
+namespace RRReddit.Data
+{
+    public class MongoDatabase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IMongoDatabase _database;
+
+        public MongoDatabase(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            var connectionString = _configuration.GetConnectionString("DbConnection");
+            var Url = MongoUrl.Create(connectionString);
+            var mongoClient = new MongoClient(Url);
+            _database = mongoClient.GetDatabase("RRReddit");
+        }
+
+        public IMongoDatabase Database => _database;
+    }
+}
